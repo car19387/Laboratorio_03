a4b9b6e040e036d1d2560c523eec008de543352d
JefryCarrasco <car19387@uvg.edu.gt>
2020-07-30 23:11:07 -0600

    ...

--- 00.Tabla01 SOP/Tabla01-SOP.v
+++ 00.Tabla01 SOP/Tabla01-SOP.v
@@ -1,14 +1,13 @@
   module Tabla01-SOP();
 
   reg A, B, C;
-  wire or01, or02, or03, or04, or05, notA, notB, notC, out;
+  wire andA, andB, andC, andD, notA, notB, notC, out;
 
-  or 1(or01, notA, notB, notC);
-  or 2(or02, notA, B, notC);
-  or 3(or03, A, notB, notC);
-  or 4(or04, A, notB, C);
-  or 5(or05, A, B, C);
-  and 1(out, or01, or02, or03, or04, or05);
+  or 1(out, andA, andB, andC, andD);
+  and 1(andA, A, B, notC);
+  and 3(andB, a, notB, notC);
+  and 2(andC, notA, notB, C);
+  and 4(andD, notA, notB, notC);
   not 1(notA, A);
   not 2(notB, B);
   not 3(notC, C);
@@ -28,6 +27,4 @@
     #1 $finish;
   end
 
-
-
-endmodule
+endmodule //Revisado
